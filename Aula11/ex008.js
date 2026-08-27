8804abc3294fad23c69c77c1af6c8fbf16ed932c
Aleyxom <aleyxom.cs.oliveira@gmail.com>
2026-08-27 10:14:06 -0300

    condições em js parte 1

---
+++ Aula11/ex008.js
@@ -0,0 +1,10 @@
+console.log("O console funcionou corretamente.")
+
+var vel = 59.5
+console.log(`A velocidade é ${vel} Km/h`)
+
+if (vel >= 60){ //if com CONDIÇÃO SIMPLES
+    console.log("Você ultrapassou o limite de velocidade permitido! MULTADO!")
+}
+    
+console.log("Dirija sempre usando o cinto de segurança!")
---
+++ Aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = "EUA"
+
+if (pais == "Brasil"){
+    console.log(`Vivendo em ${pais} você é brasileiro!`)
+}else{
+    console.log(`Vivendo em ${pais} você é estrangeiro!`)
+}
